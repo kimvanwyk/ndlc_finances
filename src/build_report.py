ae97d3f4e7ca439c54279120942b4b8f390bfa72
Kim van Wyk <kimv@sahomeloans.com>
2018-10-15 09:50:12 +0200

    Changing to latex reports

--- src/build_report.py
+++ src/build_report.py
@@ -9,10 +9,10 @@ def build_transaction_table(account, month):
     def add_row(date, desc, debit, credit):
         debit = f'{debit:.2f}' if debit else ''
         if debit and ('Balance' in desc):
-            debit = f'**{debit}**'
+            debit = f'\\textbf{{{debit}}}'
         credit = f'{credit:.2f}' if credit else ''
         if credit and ('Balance' in desc):
-            credit = f'**{credit}**'
+            credit = f'\\textbf{{{credit}}}'
         out.append((date, desc, debit, credit))
         for w in widths.keys():
             widths[w] = max(len(locals()[w]), widths[w])
@@ -22,35 +22,39 @@ def build_transaction_table(account, month):
               'debit': len('Debit'),
               'credit': len('Credit')}
     (start_balance, end_balance) = account.current_balance(month)
-    add_row(start_balance.date.strftime('%d/%m/%y'), '**Balance brought forward**', None, start_balance.amount)
+    add_row(start_balance.date.strftime('%d/%m/%y'), r'\textbf{Balance brought forward}', None, start_balance.amount)
     current_date = start_balance.date
     for t in (t for t in account.transactions if t.report_month == month):
         if current_date == t.trans_date:
-            date = '        '
+            date = ''
         else:
             current_date = t.trans_date
             date = t.trans_date.strftime('%d/%m/%y')
         add_row(date, t.description, t.amount if t.trans_type == 'payment' else None, t.amount if t.trans_type == 'deposit' else None)
-    add_row(end_balance.date.strftime('%d/%m/%y'), '**Balance of account**', None, end_balance.amount)
+    add_row(end_balance.date.strftime('%d/%m/%y'), r'\textbf{Balance of account}', None, end_balance.amount)
 
     wdesc = widths['desc']
     wdebit = widths['debit']
     wcredit = widths['credit']
     markup = []
-    markup.append(f"+{'-'*10}+{'-'*(wdesc + 2)}+{'-'*(wdebit + 2)}+{'-'*(wcredit + 2)}+")
-    markup.append(f"| {'Date':8} | {'Description':{widths['desc']}} | {'Debit':{widths['debit']}} | {'Credit':{widths['credit']}} |")
-    markup.append(f"+:{'='*9}+:{'='*(wdesc + 1)}+:{'='*(wdebit + 1)}+:{'='*(wcredit + 1)}+")
+    markup = [r'\begin{center}']
+    markup.append(r'\begin{tabularx}{\textwidth}{|c|X|r|r|}')
+    markup.append('\hhline{|-|-|-|-|}')
+    markup.append(f"\\textbf{{Date}} & \\textbf{{Description}} & \\textbf{{Debit}} & \\textbf{{Credit}} \\\\")
     for row in out:
-        markup.append(f"| {row[0]} | {row[1]:{widths['desc']}} | {row[2]:{widths['debit']}} | {row[3]:{widths['credit']}} |")
-        markup.append(f"+{'-'*10}+{'-'*(wdesc + 2)}+{'-'*(wdebit + 2)}+{'-'*(wcredit + 2)}+")
+        markup.append('\hhline{|-|-|-|-|}')
+        desc = row[1].replace('&', r'\&')
+        markup.append(f"{row[0]} & {desc} & {row[2]} & {row[3]} \\\\")
+    markup.append('\hhline{|-|-|-|-|}')
+    markup.append(r'\end{tabularx}') 
+    markup.append(r'\end{center}') 
     return markup
 
 markup = ['# Charity']
 markup.extend(build_transaction_table(Account.objects(name='charity').first(), '1810'))
 markup.append('\n# Admin')
 markup.extend(build_transaction_table(Account.objects(name='admin').first(), '1810'))
-# print('\n'.join(markup))
 with open('markup.txt', 'w') as fh:
     fh.write('\n'.join(markup))
-
+    # python build_report.py && pandoc markup.txt --template no_frills_latex.txt -o markup.pdf
         
