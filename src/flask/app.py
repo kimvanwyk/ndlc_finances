3b0763b8d8dea51580d5f6ef52f6fb397aea70ad
Kim van Wyk <kimv@sahomeloans.com>
2018-10-18 15:33:04 +0200

    Move forms to seperate file

--- src/flask/app.py
+++ src/flask/app.py
@@ -1,17 +1,14 @@
 import data.mongo_setup
-
 #from data.dues import Dues
 #from data.members import Member
 from data.cakes import CakeTransfer, CakeStock, CakePayment
 from data.transactions import Transaction, AdminTransaction, Account, AdminAccount
 from data.market import MarketMonth, MarketDay, list_market_months
 from data.members import Member, list_members
+from forms import *
 from utils import report_months
 
 from flask import Flask, render_template, url_for, redirect, flash
-from flask_wtf import FlaskForm
-from wtforms import StringField, DecimalField, DateField, RadioField, SelectField, SelectMultipleField, BooleanField, IntegerField, SubmitField
-from wtforms.validators import DataRequired
 
 from datetime import date
 
@@ -20,64 +17,6 @@ with open('secret_key.txt', 'rb') as fh:
     app.secret_key = fh.read()
 
 data.mongo_setup.global_init()
-
-class TransactionForm(FlaskForm):
-    trans_type = RadioField(label='Transaction', choices = [(c,c) for c in ('deposit', 'payment')], default='deposit')
-    trans_date = DateField(label='Date', default=date.today())
-    description = StringField(label='Description')
-    amount = DecimalField(label='Amount')
-    position = SelectField(label='Insert After')
-    report_month = SelectField(label='Report Month', choices = [(c,c) for c in report_months.get_report_months()])
-    submit = SubmitField(label='Submit New Transaction')
-
-class AdminTransactionForm(TransactionForm):
-    bar = BooleanField(label='Bar Transaction', default=False)
-
-class MarketMonthForm(FlaskForm):
-    year = IntegerField(label='Year', default=date.today().year)
-    month = IntegerField(label='Month', default=date.today().month)
-    expenses = DecimalField(label='Expenses', default=0)
-    submit = SubmitField(label='Submit New Market Month')
-
-class MarketMonthEditForm(FlaskForm):
-    month = StringField(label='Month')
-    expenses = DecimalField(label='Expenses')
-    days = SelectField(label='Market Days')
-    day_submit = SubmitField(label='Edit Market Day Details')
-    month_submit = SubmitField(label='Submit Change in Month Details')
-    new_day_submit = SubmitField(label='Add Market Day')
-
-class MarketMonthEditNoDaysForm(FlaskForm):
-    month = StringField(label='Month')
-    expenses = DecimalField(label='Expenses')
-    days = StringField(label='Market Days', default='No market days available for this month')
-    month_submit = SubmitField(label='Submit Change in Month Details')
-    new_day_submit = SubmitField(label='Add Market Day')
-
-class MarketMonthSelectorForm(FlaskForm):
-    month = SelectField(label='Market Months')
-    submit = SubmitField(label='Select Market Month')
-
-class MarketDayForm(FlaskForm):
-    date = DateField(label='Date', default=date.today())
-    members = SelectMultipleField(label='Members', choices=list_members())
-    traded = BooleanField(label='Did the market stall trade?', default=True)
-    income = DecimalField(label='Income', default=0)
-    expenses = DecimalField(label='Expenses', default=0)
-    submit = SubmitField(label='Submit Market Day Changes')
-
-class CakeTransferForm(FlaskForm):
-    date = DateField(label='Date', default=date.today())
-    number = IntegerField(label='Number of Cakes', default=12)
-    responsible_party = StringField(label='Responsible Party')
-    direction = RadioField(label='Transfer Direction', choices = [(c,c) for c in ('withdrawal', 'return')], default='withdrawal')
-    submit = SubmitField(label='Submit Cake Transfer')
-
-class CakePaymentForm(FlaskForm):
-    date = DateField(label='Date', default=date.today())
-    amount = DecimalField(label='Amount')
-    responsible_party = StringField(label='Responsible Party')
-    submit = SubmitField(label='Submit Cake Payment')
 
 account_map = {'charity': (Account, Transaction, TransactionForm),
                'admin': (AdminAccount, AdminTransaction, AdminTransactionForm)
