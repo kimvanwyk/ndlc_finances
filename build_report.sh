fee29d894726dcb0125b4c8a0c6ef62c04e136f6
Kim van Wyk <kimv@sahomeloans.com>
2018-10-21 21:48:19 +0200

    Add port

--- build_report.sh
+++ build_report.sh
@@ -1,1 +1,1 @@
-docker run --network host --rm -it -v/home/kimv/src/ndlc_finances:/io kimvanwyk/ndlc_finance_report
+docker run --network host -p 5001:5001 --rm -it -v/home/kimv/src/ndlc_finances:/io kimvanwyk/ndlc_finance_report
