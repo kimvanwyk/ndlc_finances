e6882d848cadb945cb9dfec83f3a508d5920c568
Kim van Wyk <kimv@sahomeloans.com>
2018-10-22 16:58:40 +0200

    Move file after creation. Changes will only function inside a container

--- src/interface.py
+++ src/interface.py
@@ -1,5 +1,6 @@
 from datetime import date
 import os, os.path
+import shutil
 import socket
 
 import build_report
@@ -10,12 +11,16 @@ UDP_PORT = 5001
 
 def build(verbose=True):
     text = build_report.build_markup_file()
-    (ret, retcode) = kppe.build_pdf(text, os.path.abspath('templates/no_frills_latex.txt'), f'{date.today():%y%m%d}_ndlc_finance_report', toc=False)
+    fn = f'{date.today():%y%m%d}_ndlc_finance_report'
+    (ret, retcode) = kppe.build_pdf(text, os.path.abspath('templates/no_frills_latex.txt'), fn, toc=False)
     if verbose:
         print('Pandoc output:')
         print()
         print(ret)
+    if retcode == 0:
+        shutil.move(f'/app/{fn}.pdf', '/io')
     return (ret, retcode)
+
 
 if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET,
